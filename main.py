38556231d502a41c06e3906d0a52d4b07bfdecf5
Kanhu charan Moharana <kanhu-pcr-code@users.noreply.github.com>
2019-01-25 18:13:21 -0200

    update

--- main.py
+++ main.py
@@ -4,14 +4,14 @@
   Find the individuals with all 1
 '''
 import sys,os,re
-import Operon_OOP
+import simple_GA
 import numpy as np
 import matplotlib.pyplot as plt
 
 def main():
  print("#### Genetic Algorithm ####")
  NGEN,NPOP,CHROM_SIZE, MUT_PROB, CROSS_PROB = 10,10,5,0.01,0.5
- pop = Operon_OOP.population(n=NPOP, k= CHROM_SIZE)
+ pop = simple_GA.population(n=NPOP, k= CHROM_SIZE)
  pop.random_populate()
  pop.evaluate()
  pop.view(sort=True)
@@ -27,11 +27,11 @@ def main():
   plt.plot(range(1,len(top_score_per_gen)+1), top_score_per_gen, color='blue', marker='.')
   plt.xlim(1,NGEN)
   plt.pause(0.05)
-  new_pop = Operon_OOP.population(n=NPOP, k= CHROM_SIZE)
+  new_pop = simple_GA.population(n=NPOP, k= CHROM_SIZE)
   for _ in range(NPOP):
    c = pop.select(tournsize=4,k=2)
-   offspring = Operon_OOP.cross_over(c[0],c[1],CROSS_PROB)
-   c = Operon_OOP.mutate_chromosome(offspring,MUT_PROB)
+   offspring = simple_GA.cross_over(c[0],c[1],CROSS_PROB)
+   c = simple_GA.mutate_chromosome(offspring,MUT_PROB)
    new_pop.add_chromosome(c)
   new_pop.evaluate()
   #new_pop.view(sort=True)
